772fbb66d4fbb312083627c4f9679385d4056d08
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-07-25 15:14:04 +0530

    scaffold the app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles/main.scss';
+import 'rsuite/dist/styles/rsuite-default.css';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
