dd1abf2b818363d56a62ec2ed55d82d2791512bb
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-07-25 15:55:06 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyB2xBYMjSgBQIAe1EAQQ8O0eZRWQJaIxwc',
+  authDomain: 'chat-web-app-44a3a.firebaseapp.com',
+  projectId: 'chat-web-app-44a3a',
+  storageBucket: 'chat-web-app-44a3a.appspot.com',
+  messagingSenderId: '580943048736',
+  appId: '1:580943048736:web:9ca640f741d5e12a310c45',
+};
+
+const app = firebase.initializeApp(config);
