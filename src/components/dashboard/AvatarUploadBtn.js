47a6c14af9f8f0a5fb18a457abf7a66cb152a6d2
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-07-30 11:58:18 +0530

    displayed messages inside chat page

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -5,6 +5,7 @@ import AvatarEditor from 'react-avatar-editor';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helpers';
 
 const fileInputTpes = '.png, .jpg ,.jpeg';
 
@@ -60,10 +61,16 @@ const AvatarUploadBtn = () => {
       });
 
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
-      userAvatarRef.set(downloadUrl);
+
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
+
+      await database.ref().update(updates);
+
       setIsLoading(false);
       Alert.success('Upload Successfull', 4000);
     } catch (err) {
