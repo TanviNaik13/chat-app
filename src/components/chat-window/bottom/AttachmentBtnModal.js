7264a8a7ba97ccd1d20bca2f238730411056a74d
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-08-01 20:41:17 +0530

    created uploader component

---
+++ src/components/chat-window/bottom/AttachmentBtnModal.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { Alert, Button, Icon, InputGroup, Modal, Uploader } from 'rsuite';
+import InputGroupButton from 'rsuite/lib/InputGroup/InputGroupButton';
+import { useModalState } from '../../../misc/custom-hooks';
+import { upload } from '@testing-library/user-event/dist/upload';
+import { storage } from '../../../misc/firebase';
+import { useParams } from 'react-router-dom';
+
+const MAX_FILE_SIZE = 1000 * 1024 * 5;
+
+const AttachmentBtnModal = ({ afterUpload }) => {
+  const { chatId } = useParams();
+  const { isOpen, open, close } = useModalState();
+  const [fileList, setFileList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const onChange = fileArr => {
+    const filtered = fileArr
+      .filter(el => el.blobFile.size <= MAX_FILE_SIZE)
+      .slice(0, 5);
+    setFileList(filtered);
+  };
+
+  const onUpload = async () => {
+    try {
+      const uploadPromises = fileList.map(f => {
+        storage
+          .ref(`/chat/${chatId}`)
+          .child(Date.now() + f.name)
+          .put(f.blobFile, {
+            cacheControl: `public, max-age=${3600 * 24 * 3}`,
+          });
+      });
+      const uploadSnapshots = await Promise.all(uploadPromises);
+
+      const shapePromises = uploadSnapshots.map(async snap => {
+        return {
+          contentType: snap.metadata.contentType,
+          name: snap.metadata.name,
+          url: await snap.ref.getDownloadURL(),
+        };
+      });
+      const files = await Promise.all(shapePromises);
+
+      await afterUpload(files);
+      setLoading(false);
+      close();
+    } catch (err) {
+      setLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <InputGroup.Button onClick={open}>
+        <Icon icon="attachment" />
+      </InputGroup.Button>
+
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>Upload Files</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Uploader
+            fileList={fileList}
+            autoUpload={false}
+            action=""
+            onChange={onChange}
+            multiple
+            listType="picture-text"
+            className="w-100"
+            disabled={loading}
+          />
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button block disabled={loading} onClick={upload}>
+            Send to chat
+          </Button>
+          <div className="text-right mt-2">
+            <small>* files less than 5 MB only</small>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default AttachmentBtnModal;
--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/app';
 import { useProfile } from '../../../context/profile.context';
 import { useParams } from 'react-router-dom';
 import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
 
 function assembleMessages(profile, chatId) {
   return {
@@ -66,6 +67,7 @@ const Bottom = () => {
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal />
         <Input
           placeholder="Write a new message here"
           value={input}
