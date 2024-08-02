4eb5d9872f2e114310dfc7e92a0cc916ee8e3b78
Tanvi Naik <21co67@aitdgoa.edu.in>
2024-08-02 23:15:40 +0530

    installing and adding nvm for nodejs v10.20.1

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable quotes */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://chat-web-app-44a3a-default-rtdb.firebaseio.com',
+});
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
